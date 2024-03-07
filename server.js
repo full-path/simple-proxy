558a6ff9e7edeec0c61f38eca596a61040b89a5a
Nome <dickerson.nomi@gmail.com>
2024-03-07 08:41:07 -0800

    no this

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const TARGET_URL = process.env.TARGET_URL;
 
-app.use(bodyParser.text({type:'*/*'}));
+app.use(express.bodyParser.text({type:'*/*'}));
 
 function constructApiUrl(query, baseUrl) {
   const queryString = new URLSearchParams(query).toString();
